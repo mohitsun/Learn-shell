0745f200a7c0ed3b742a5500e2e47a18061e0a6f
iamdevopstrainer <iamdevopstrainer@gmail.com>
2023-07-26 01:40:42 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,6 @@ yum install nginx -y
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 rm -rf /usr/share/nginx/html/*
-
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
