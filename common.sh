cf6dbfea918fabed09090882faa904685f7c884a
iamdevopstrainer <iamdevopstrainer@gmail.com>
2023-08-20 13:44:25 +0530

    frontend

--- common.sh
+++ common.sh
@@ -117,6 +117,7 @@ func_python(){
   yum install python36 gcc python3-devel -y &>>${log}
 
   func_apppreq
+  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
 
   echo -e "\e[36m>>>>>>>>>>>>> Build ${component} service <<<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
--- payment.sh
+++ payment.sh
@@ -1,4 +1,9 @@
 component=payment
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+  echo Input Rabbitmq app user password missing
+  exit 1
+fi
 source common.sh
 
 func_python
