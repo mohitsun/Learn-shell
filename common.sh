60a87ff635d153c5fc5b67d4d95fe8348052d624
iamdevopstrainer <iamdevopstrainer@gmail.com>
2023-08-20 12:18:18 +0530

    Roboshop scripts 1

--- common.sh
+++ common.sh
@@ -3,41 +3,50 @@ log=/tmp/roboshop.log
 func_apppreq(){
   echo -e "\e[36m>>>>>>>>>>>>> Create ${component} service <<<<<<<<<<<<<\e[0m"
     cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+    echo $?
 
   echo -e "\e[36m>>>>>>>>>>>>> Create application user <<<<<<<<<<<<<\e[0m"
     useradd roboshop &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>>>> cleanup existing application content <<<<<<<<<<<<<\e[0m"
-    rm -rf /app &>>${log}
+    rm -rf /app &>>${log
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>>>> Create application directory <<<<<<<<<<<<<\e[0m"
     mkdir /app &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>>>> download application content <<<<<<<<<<<<<\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<\e[0m"
     cd /app
     unzip /tmp/${component}.zip &>>${log}
-    cd /app
+    echo $?
 }
 
 func_schema_setup(){
   if [ "${schema_type}" == "mongodb" ]; then
     echo -e "\e[36m>>>>>>>>>>>>> Install Mongo client <<<<<<<<<<<<<\e[0m"
     yum install mongodb-org-shell -y &>>${log}
+    echo $?
 
     echo -e "\e[36m>>>>>>>>>>>>> Load ${component} schema <<<<<<<<<<<<<\e[0m"
     mongo --host mongodb.mohdevops.online </app/schema/${component}.js &>>${log}
+    echo $?
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
       echo -e "\e[36m>>>>>>>>>>>>> Install mysql client<<<<<<<<<<<<<\e[0m"
       yum install mysql -y &>>${log}
+      echo $?
       # shellcheck disable=SC2261
 
       echo -e "\e[36m>>>>>>>>>>>>> Load schema<<<<<<<<<<<<<\e[0m"
       mysql -h mysql.mohdevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+      echo $?
   fi
 }
 func_systemd(){
@@ -45,6 +54,7 @@ func_systemd(){
      systemctl daemon-reload &>>${log}
      systemctl enable ${component} &>>${log}
      systemctl start ${component} &>>${log}
+     echo $?
 }
 
 nodejs(){
@@ -54,16 +64,21 @@ nodejs(){
   echo -e "\e[36m>>>>>>>>>>>>> Create MongoDB repo <<<<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
+  echo $?
+
   echo -e "\e[36m>>>>>>>>>>>>> Install  NodeJS repos <<<<<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  echo $?
 
   echo -e "\e[36m>>>>>>>>>>>>> Install NodeJS <<<<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
+  echo $?
 
   func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>> Download NodeJS dependencies <<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
+  echo $?
 
   func_schema_setup
 
